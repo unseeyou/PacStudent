68207a5af88b9c0b512ed75dc8a8d6b58b8a4206
unseeyou <unseeyoubestmod@gmail.com>
2026-09-21 20:26:29 +1000

    some of the corners like simple boxes work now but the T shapes are still tough

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -29,7 +29,34 @@ public class LevelGenerator : MonoBehaviour
     
     bool IsWall(int id)
     {
-        return id == 1 || id == 2 || id == 3 || id == 4 || id == 7;
+        return id is 1 or 2 or 3 or 4 or 7;
+    }
+    
+    float GetTileRotation(int[,] grid, int r, int c, int tileID)
+    {
+        int rows = grid.GetLength(0);
+        int cols = grid.GetLength(1);
+        
+        bool top = (r > 0) && IsWall(grid[r - 1, c]);
+        bool bottom = (r < rows - 1) && IsWall(grid[r + 1, c]);
+        bool left = (c > 0) && IsWall(grid[r, c - 1]);
+        bool right = (c < cols - 1) && IsWall(grid[r, c + 1]);
+        
+        if (tileID == 2 || tileID == 4)
+        {
+            if (top || bottom) return 90f;
+            return 0f;                    
+        }
+        
+        if (tileID == 1 ||  tileID == 3)
+        {
+            if (top && right)    return 90f;
+            if (right && bottom) return 0f;
+            if (bottom && left)  return -90;
+            if (left && top)     return 180f;
+        }
+
+        return 0f;
     }
 
     TileBase GetTileFromID(int tileID)
@@ -40,10 +67,10 @@ public class LevelGenerator : MonoBehaviour
             switch (tileID)
             {
                 case 1:
-                    tileToPlace = tiles[3];
+                    tileToPlace = tiles[2];
                     break;
                 case 2:
-                    tileToPlace = tiles[2];
+                    tileToPlace = tiles[3];
                     break;
                 case 3:
                     tileToPlace = tiles[0];
@@ -67,10 +94,6 @@ public class LevelGenerator : MonoBehaviour
 
     void PlaceTiles(int[,] grid)
     {
-        foreach (var tile in tiles)
-        {
-            Debug.Log($"LevelGenerator: Using saved tile {tile.name}");
-        }
         int rows = grid.GetLength(0);
         int cols = grid.GetLength(1);
 
@@ -105,6 +128,12 @@ public class LevelGenerator : MonoBehaviour
                     int xPos = c - Mathf.CeilToInt(cols/2f);
                     Vector3Int pos = new Vector3Int(xPos, yPos, 0);
                     target.SetTile(pos, tileToPlace);
+                    target.SetTileFlags(pos, TileFlags.None);
+                    
+                    float rotAngle = GetTileRotation(grid, r, c, tileID);
+                    Quaternion rot = Quaternion.Euler(0f, 0f, rotAngle);
+                    Matrix4x4 mat = Matrix4x4.TRS(Vector3.zero, rot, Vector3.one);
+                    target.SetTransformMatrix(pos, mat);
                 }
             }
         }
