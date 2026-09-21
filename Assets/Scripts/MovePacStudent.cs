d9fe26ccf7f49d0340d1921ec9a35147b3961180
unseeyou <unseeyoubestmod@gmail.com>
2026-09-21 11:59:19 +1000

    full map

---
+++ Assets/Scripts/MovePacStudent.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MovePacStudent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform target;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
